9b59be29009f60ceade73397a93d59fb59a13ce1
InyeobChoi <38967508+InyeobChoi@users.noreply.github.com>
2024-08-06 20:29:54 -0700

    fix: side bar menu

---
+++ nextjs-admin/src/components/Sidebar/SidebarItem.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Link from "next/link";
+
+const SidebarItem = () => {
+  const handleClick = () => {};
+  return (
+    <>
+      <li>
+        
+      </li>
+    </>
+  );
+};
+
+export default SidebarItem;
