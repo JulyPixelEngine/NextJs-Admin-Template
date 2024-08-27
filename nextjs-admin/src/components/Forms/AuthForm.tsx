1abedf66ff0aad686acac953af2f601f69571778
InyeobChoi <38967508+InyeobChoi@users.noreply.github.com>
2024-08-26 20:04:13 -0700

    fix: form

--- nextjs-admin/src/components/Forms/AuthForm.tsx
+++ nextjs-admin/src/components/Forms/AuthForm.tsx
@@ -1,4 +1,32 @@
 'use client'
 
 import { useState } from 'react'
-// import { useForm } from 'react-hook-form'
+import { useForm } from 'react-hook-form'
+import { signIn } from 'next-auth/react';
+import * as z from zod;
+import { useSearchParams } from 'next/navigation';
+
+const formSchema = z.object({
+    email : z.string().email({message : "Enter a valid email address"})
+})
+
+type UserFormValue = z.infer<typeof formSchema>;
+
+export default function UserAuthForm() {
+    const searchParam = useSearchParams();
+    const callbackUrl = searchParam.get('callbackUrl');
+
+    const dafaultEmail = {
+        email: 'demo@gmail.com'
+    };
+    const onSubmit = async(data: UserFormValue) => {
+        signIn('credentials', {
+            email: data.email,
+            callbackUrl: callbackUrl ?? '/dashboard'
+        })
+    }
+    return (
+        <>
+        </>
+    )
+}
---
+++ nextjs-admin/src/components/common/Form.tsx
@@ -0,0 +1,14 @@
+import { Controller, ControllerProps, FieldValues, FormProvider } from "react-hook-form";
+
+import { Slot } from '@radix-ui/react-slot'
+import * as LabelPrimitive from '@radix-ui/react-label'
+
+
+const Form = FormProvider;
+
+const userFormField = () => {
+
+}
+export {
+    userFormField
+}
