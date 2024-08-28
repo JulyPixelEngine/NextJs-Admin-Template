f137dfc84540341ee01cf6611fa18b29683175eb
InyeobChoi <38967508+InyeobChoi@users.noreply.github.com>
2024-08-27 21:38:06 -0700

    fix: context study

--- nextjs-admin/src/components/common/Form.tsx
+++ nextjs-admin/src/components/common/Form.tsx
@@ -3,12 +3,23 @@ import { Controller, ControllerProps, FieldValues, FormProvider } from "react-ho
 import { Slot } from '@radix-ui/react-slot'
 import * as LabelPrimitive from '@radix-ui/react-label'
 
+import React from "react";
+
 
 const Form = FormProvider;
 
 const userFormField = () => {
 
 }
+
+const FormItemContext = React.createContext<FormItemContextValue>(
+    {} as FormItemContextValue
+);
+
+type FormItemContextValue = {
+
+};
+
 export {
     userFormField
 }
