7bb951c89eae061ce4a947ae887320ef9c5df985
InyeobChoi <38967508+InyeobChoi@users.noreply.github.com>
2024-09-03 21:46:01 -0700

    fix: react button

--- nextjs-admin/src/components/Forms/AuthForm.tsx
+++ nextjs-admin/src/components/Forms/AuthForm.tsx
@@ -14,6 +14,8 @@ import {
     Form
 } from '@/components/Common/Form'
 
+import { Button } from '@/components/Common/UI/Button'
+
 const formSchema = z.object({
     email : z.string().email({message : "Enter a valid email address"})
 })
@@ -23,6 +25,7 @@ type UserFormValue = z.infer<typeof formSchema>;
 export default function UserAuthForm() {
     const searchParam = useSearchParams();
     const callbackUrl = searchParam.get('callbackUrl');
+    const [loading, setLoading] = useState(false);
 
     const defaultValues = {
         email: 'demo@gmail.com'
@@ -38,15 +41,22 @@ export default function UserAuthForm() {
         })
     }
     return (
-        <>
-            <Form {...form}>
-                       
-                <form onSubmit={form.handleSubmit(onSubmit)}
-                    className='w-full'
-                >
-                    <button> Continue with Email</button>
-                </form>
-            </Form>
-        </>
-    )
+      <>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="w-full">
+            <Button disabled={loading} className="ml-auto w-full" type="submit">
+              Continue With Email
+            </Button>
+          </form>
+        </Form>
+        <div className="relative">
+          <div>
+            <span></span>
+          </div>
+          <div>
+            <span>Or Continue With</span>
+          </div>
+        </div>
+      </>
+    );
 }
--- nextjs-admin/src/components/Layouts/LandingLayout.tsx
+++ nextjs-admin/src/components/Layouts/LandingLayout.tsx
@@ -11,16 +11,60 @@ export default async function Home() {
     const session = (await getServerSession()) || {};
     return (
       <div className="relative h-screen flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
-        <Link href={"/"}>Login</Link>
         <div className="relative hidden h-full flex-col bg-muted p-10 text-white lg:flex dark:border-r">
           <div className="relative z-20 flex items-center text-lg font-medium">
-            <svg width="200" height="200" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
-                <rect width="100" height="100" fill="black" />
-                <rect x="30" y="35" width="40" height="30" rx="10" fill="none" stroke="white" strokeWidth="5" />
-                <path d="M45 45 L40 50 L45 55" stroke="white" strokeWidth="5" fill="none" />
-                <path d="M55 45 L60 50 L55 55" stroke="white" strokeWidth="5" fill="none" />
-                <text x="50%" y="80%" dominantBaseline="middle" textAnchor="middle" fontFamily="Arial, sans-serif" fontSize="13" fill="white" fontWeight="bold">bytecode</text>
-                <text x="50%" y="95%" dominantBaseline="middle" textAnchor="middle" fontFamily="Arial, sans-serif" fontSize="5" fill="white">WEB PROGRAMMING</text>
+            <svg
+              width="200"
+              height="200"
+              viewBox="0 0 100 100"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <rect width="100" height="100" fill="black" />
+              <rect
+                x="30"
+                y="35"
+                width="40"
+                height="30"
+                rx="10"
+                fill="none"
+                stroke="white"
+                strokeWidth="5"
+              />
+              <path
+                d="M45 45 L40 50 L45 55"
+                stroke="white"
+                strokeWidth="5"
+                fill="none"
+              />
+              <path
+                d="M55 45 L60 50 L55 55"
+                stroke="white"
+                strokeWidth="5"
+                fill="none"
+              />
+              <text
+                x="50%"
+                y="80%"
+                dominantBaseline="middle"
+                textAnchor="middle"
+                fontFamily="Arial, sans-serif"
+                fontSize="13"
+                fill="white"
+                fontWeight="bold"
+              >
+                bytecode
+              </text>
+              <text
+                x="50%"
+                y="95%"
+                dominantBaseline="middle"
+                textAnchor="middle"
+                fontFamily="Arial, sans-serif"
+                fontSize="5"
+                fill="white"
+              >
+                WEB PROGRAMMING
+              </text>
             </svg>
             Logo
           </div>
@@ -29,6 +73,10 @@ export default async function Home() {
               <p className="text-lg">Design Text</p>
             </blockquote>
           </div>
+        </div>
+        <Link href={"/"}>Login</Link>
+        <div>
+          
         </div>
         <div className="flex h-full items-center p-4 lg:p-8">
           <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
@@ -41,7 +89,7 @@ export default async function Home() {
               </p>
             </div>
 
-            <AuthForm/>
+            <AuthForm />
             <p className="px-8 text-center text-sm">
               By clicking continue, you agree to our
             </p>
